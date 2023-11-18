020719ce2c3e8a85e07a4d704abef0bc5f33a2e9
Jozef Ferko <jozefferko9@gmail.com>
2023-11-18 15:18:03 +0100

    add TS version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # facebook-notifier
-Scrapes a facebook group page and emails pictures of new listings
+Scrapes a facebook group and emails you new posts
+
+# <span style="color:red">warning! meant to be run locally! credentials are not protected! </span>.
---
+++ typescript_version/src/main.ts
@@ -0,0 +1,86 @@
+
+import { PlaywrightCrawler } from 'crawlee';
+import nodemailer from 'nodemailer';
+const FACEBOOK_USERNAME = ''
+const FACEBOOK_PASSWORD = ''
+const FACEBOOK_GROUP_ID = ''
+const GMAIL_ADDRESS = ''
+const GMAIL_PASSWORD = ''
+const REFRESH_FREQUENCY = 10 //in minutes, if its too low, you'll get banned
+
+let previousName = "";
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: GMAIL_ADDRESS,
+        pass: GMAIL_PASSWORD
+    }
+})
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const crawler = new PlaywrightCrawler({
+    async requestHandler({ request, page, enqueueLinks, log }) {
+        if (request.label === "fbgroup") {
+            const title = await page.title();
+            log.info(`Title of ${request.loadedUrl} is '${title}'`);
+
+            const currentName = await page.locator("//div[@role='feed']/div[2]//strong[1]/span").nth(0).textContent();
+            log.info(`Current top post is from '${currentName}'`);
+
+            if (previousName !== currentName) {
+                previousName = currentName || "";
+
+                const link = await page.locator("//div[@role='feed']/div[2]//a").nth(3).getAttribute("href");
+                await page.locator("//div[@role='feed']/div[2]").nth(0).screenshot({ path: 'screenshot.png' });
+
+                // email
+                try {
+                    await transporter.sendMail({
+                        from: '"Message bot"<your@gmail.com>',
+                        to: GMAIL_ADDRESS,
+                        subject: `NEW LISTING FROM ${currentName}`,
+                        html: `<a href="${link}"><img src="cid:screenshot"></a>`,
+                        attachments: [{
+                            filename: 'screenshot.png',
+                            path: 'screenshot.png',
+                            cid: 'screenshot'
+                        }]
+                    })
+                } catch (err) {
+                    console.log(err)
+                }
+
+            }
+            await delay(REFRESH_FREQUENCY * 1000 * 60)
+            await enqueueLinks({
+                urls: [`http://www.facebook.com/groups/${FACEBOOK_GROUP_ID}/?sorting_setting=CHRONOLOGICAL`],
+                label: "fbgroup"
+            });
+
+        } else {
+            //auth section
+            const title = await page.title();
+            log.info(`Title of ${request.loadedUrl} is '${title}'`);
+
+            await page.getByTestId('cookie-policy-manage-dialog-accept-button').nth(0).click();
+            await page.fill('input[name="email"]', FACEBOOK_USERNAME);
+            await page.fill('input[name="pass"]', FACEBOOK_PASSWORD);
+            await page.click('button[name="login"]');
+            // page.getByRole("search");
+            // page.locator("button[aria-label='Messenger']")
+            await delay(10000)
+            await enqueueLinks({
+                urls: [`http://www.facebook.com/groups/${FACEBOOK_GROUP_ID}/?sorting_setting=CHRONOLOGICAL`],
+                label: "fbgroup"
+            });
+        }
+    },
+    // Uncomment this option to see the browser window.
+    // headless: false,
+});
+
+await crawler.run(['http://www.facebook.com']);
